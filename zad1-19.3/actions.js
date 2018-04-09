e53fd808ddf5d7dd12200c77baa4196a23279ffb
Malgorzata Kudlak <m.kudlak@icloud.com>
2018-04-09 08:13:45 +0200

    Add files add one type of action add comment

---
+++ zad1-19.3/actions.js
@@ -0,0 +1,13 @@
+const ADD_COMMENT = 'ADD_COMMENT';
+const REMOVE_COMMENT = 'REMOVE_COMMENT';
+const EDIT_COMMENT = 'EDIT_COMMENT';
+const THUMB_UP_COMMENT = 'THUMB_UP_COMMENT';
+const THUMB_DOWN_COMMENT = 'THUMB_DOWN_COMMENT';
+
+
+function addComment(text) {
+    return {
+        type: ADD_COMMENT,
+        text,
+    }
+}
