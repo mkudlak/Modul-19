cd06009145453b7e1241a2ddc1f522b33d4750f3
Malgorzata Kudlak <m.kudlak@icloud.com>
2018-04-11 20:21:35 +0200

    Add uuid, package.json and gitignore, add new actions.js with new function addComment, removeComment, thumbUp, thumbDown

--- zad1-19.3/actions.js
+++ zad1-19.3/actions.js
@@ -1,13 +1,43 @@
-const ADD_COMMENT = 'ADD_COMMENT';
-const REMOVE_COMMENT = 'REMOVE_COMMENT';
-const EDIT_COMMENT = 'EDIT_COMMENT';
-const THUMB_UP_COMMENT = 'THUMB_UP_COMMENT';
-const THUMB_DOWN_COMMENT = 'THUMB_DOWN_COMMENT';
+import uuid from uuid;
+
+//action types
+export const ADD_COMMENT = 'ADD_COMMENT';
+export const REMOVE_COMMENT = 'REMOVE_COMMENT';
+export const EDIT_COMMENT = 'EDIT_COMMENT';
+export const THUMB_UP_COMMENT = 'THUMB_UP_COMMENT';
+export const THUMB_DOWN_COMMENT = 'THUMB_DOWN_COMMENT';
 
 
-function addComment(text) {
+//action creators
+export function addComment(text) {
     return {
         type: ADD_COMMENT,
         text,
+        id: uuid.v4()
+    }
+}
+export function removeComment(id) {
+    return {
+        type: REMOVE_COMMENT,
+        id
+    }
+}
+export function editComment(text, id) {
+    return {
+        type: EDIT_COMMENT,
+        text,
+        id
+    }
+}
+export function thumbUpComment(id) {
+    return {
+        type: THUMB_UP_COMMENT,
+        id
+    }
+}
+export function thumbDownComment(id) {
+    return {
+        type: THUMB_DOWN_COMMENT,
+        id
     }
 }
